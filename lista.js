523746c9c5e611b9e5ea9b94396b7e5b854b8213
Cintia Souza <cintiasds1@gmail.com>
2023-06-13 21:07:09 -0300

    Em construção

--- lista.js
+++ lista.js
@@ -1,27 +1,33 @@
 function clique(){
-    var li = document.createElement('li');
+    var li = document.createElement('LI');
     var input = document.form_p.tarefa.value;
     var input_text = document.createTextNode(input);
+
     li.appendChild(input_text);
     document.querySelector('ul').appendChild(li);
-    document.form_p.tarefa = "";
+    document.form_p.tarefa.value = "";
 
     createCloseButton(li);
 
+
     function createCloseButton(li){
-        var span = document.createElement('span');
-        var txt = document.createElement('txt1');
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+
         span.className = "close";
         span.appendChild(txt);
         li.appendChild(span);
+
         span.onclick = () =>
         span.parentElement.style.display = "none";
     }
 
     document.querySelectorAll('li').forEach(createCloseButton);
+    
     document.querySelector('ul').addEventListener('click',(e) => {
-        if(e.target.tagName === 'li')
+        if(e.target.tagName === 'LI')
         e.target.classList.toggle('checked');
-    });
+      });
 
+     
 }
